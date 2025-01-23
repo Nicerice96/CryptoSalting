9c841e52a6e1aceab9a04de0a7b0be7ac5ed385d
Nicerice96 <100174834+Nicerice96@users.noreply.github.com>
2025-01-23 14:28:08 -0500

    okay so the most common password file did not work...

--- part1.py
+++ part1.py
@@ -1,17 +1,38 @@
 import hashlib
 
+student_number = "101224172"
+last_two_digits = student_number[-2:]
+target = f"c0ffee{last_two_digits}"
+def open_file():
+    file = open("10-million-password-list-top-1000000.txt", "r")
+    
+    return file.readlines()
+    
+    
 
-def generate_hash():
+def generate_hash(password_list):
     
     salt = "101224172"
-    password = ""
-    hashed_password = hashlib.sha256(password.encode() + salt.encode()).hexdigest()
     
-    return hashed_password
+    for password in password_list:
+        password = password.strip()
+        hashed_password = hashlib.sha256(password.encode() + salt.encode()).hexdigest
+        
+        if "target" in str(hashed_password):
+            return True
+        
+    return False
+
+
+    
     
     
 if __name__ == "__main__":
-    hashed_password = generate_hash()
     
-    print(hashed_password)
+    password_list = open_file()
+    
+    print(generate_hash(password_list=password_list))
+    
+    
+
     
